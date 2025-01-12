92c4a19df38527f326717d5aaa8e2382d3c2b1f1
Eagler <jayden.okubadej@outlook.com>
2025-01-11 20:42:50 -0600

    test eagler install

--- assets/mods/test.js
+++ assets/mods/test.js
@@ -1,12 +1,55 @@
-// Function to run the mod code
-function runMod() {
-    // Generate a random number between 1 and 10
-    let randomNumber = Math.floor(Math.random() * 10) + 1;
-  
-    // Display the number in an alert box
-    alert("The random number is: " + randomNumber);
-  }
-  
-  // Example of how to use it in your mod script
-  runMod();  // This will execute the mod code for the first time
-  
+(() => {
+    ModAPI.meta.title("Timescale Command");
+    ModAPI.meta.description("/timescale 0.5 to halve the speed of time");
+    ModAPI.meta.credits("By ZXMushroom63");
+    PluginAPI.addEventListener("sendchatmessage", (event) => {
+        if (event.message.toLowerCase().startsWith("/timescale")) {
+            var speed = parseFloat(event.message.split(" ")[1]);
+            if (!speed) {
+                speed = 1;
+                PluginAPI.mc.timer.timerSpeed = 1;
+            } else {
+                if (speed < 1) {
+                    speed = 1 / Math.round(1 / speed);
+                } else {
+                    speed = Math.round(speed);
+                }
+                PluginAPI.mc.timer.timerSpeed = speed;
+            }
+            PluginAPI.displayToChat("[Timescale] Set world timescale to " + speed.toFixed(2) + ".");
+        }
+    });
+    PluginAPI.dedicatedServer.appendCode(function () {
+        globalThis.timeScale = 1n;
+        globalThis.timeScaleDividing = false;
+        PluginAPI.addEventListener("processcommand", (event) => {
+            if (event.command.toLowerCase().startsWith("/timescale")) {
+                var speed = parseFloat(event.command.split(" ")[1]);
+                if (!speed) {
+                    globalThis.timeScale = 1n;
+                    globalThis.timeScaleDividing = false;
+                } else {
+                    if (speed < 1) {
+                        globalThis.timeScaleDividing = true;
+                        globalThis.timeScale = BigInt(Math.round(1 / speed));
+                    } else {
+                        globalThis.timeScaleDividing = false;
+                        globalThis.timeScale = BigInt(Math.round(speed));
+                    }
+                }
+                if (ModAPI.server) {
+                    ModAPI.server.currentTime = PluginAPI.hooks.methods[ModAPI.util.getMethodFromPackage("net.minecraft.server.MinecraftServer", "getCurrentTimeMillis")]();
+                }
+                event.preventDefault = true;
+            }
+        });
+        const original_getCurrentTime = ModAPI.hooks.methods[ModAPI.util.getMethodFromPackage("net.minecraft.server.MinecraftServer", "getCurrentTimeMillis")];
+        PluginAPI.hooks.methods[ModAPI.util.getMethodFromPackage("net.minecraft.server.MinecraftServer", "getCurrentTimeMillis")] = function () {
+            if (globalThis.timeScaleDividing) {
+                return original_getCurrentTime() / globalThis.timeScale;
+            } else {
+                return original_getCurrentTime() * globalThis.timeScale;
+            }
+        };
+    });
+})();
