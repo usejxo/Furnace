1ff2d4cf8725c880fb572df3b632e14dc313e138
Eagler <jayden.okubadej@outlook.com>
2025-01-11 18:58:29 -0600

    test js function

---
+++ assets/mods/test.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 10
+let randomNumber = Math.floor(Math.random() * 10) + 1; 
+
+// Display the number in an alert box
+alert("The random number is: " + randomNumber); 
