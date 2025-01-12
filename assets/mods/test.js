4297097a8f449ed53eeb3d3c05ffd5b5db2ffbc2
Eagler <jayden.okubadej@outlook.com>
2025-01-11 19:59:23 -0600

    testing

--- assets/mods/test.js
+++ assets/mods/test.js
@@ -1,5 +1,12 @@
-// Generate a random number between 1 and 10
-let randomNumber = Math.floor(Math.random() * 10) + 1; 
-
-// Display the number in an alert box
-alert("The random number is: " + randomNumber); 
+// Function to run the mod code
+function runMod() {
+    // Generate a random number between 1 and 10
+    let randomNumber = Math.floor(Math.random() * 10) + 1;
+  
+    // Display the number in an alert box
+    alert("The random number is: " + randomNumber);
+  }
+  
+  // Example of how to use it in your mod script
+  runMod();  // This will execute the mod code for the first time
+  
